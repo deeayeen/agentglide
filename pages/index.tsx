cb6e4a1b42a25ebc35379c1e228d6de089b0f048
Dan <35680780+deeayeen@users.noreply.github.com>
2024-10-06 20:34:01 +0300

    add CTA

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Map from "@/components/map-3d/map";
+import { Button } from "@/components/ui/button";
+import { CircleArrowRightIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,6 +17,17 @@ export default function Home() {
             className="mr-2"
           />
           <div className="text-xl font-black select-none">agentglide</div>
+        </div>
+      </div>
+      <div className="fixed bottom-0 bottom-0 flex justify-center w-screen">
+        <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-t-lg px-6 py-4">
+          <div className="text-3xl font-black select-none">
+            photo-realistic 3D AI travel agent
+          </div>
+          <Button className="mt-3 w-full bg-white text-black hover:bg-gray-200 hover:text-black font-bold text-lg">
+            where would you like to travel?{" "}
+            <CircleArrowRightIcon className="w-5 h-5 ml-2" />
+          </Button>
         </div>
       </div>
     </div>
